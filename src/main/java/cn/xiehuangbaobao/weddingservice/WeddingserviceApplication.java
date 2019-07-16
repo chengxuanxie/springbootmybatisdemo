433936330b85dbdf0c987142d86427fc20ed3359
Janna <504162491@qq.com>
2019-07-16 22:53:27 +0800

    feat(0.0.1):create demo

---
+++ src/main/java/cn/xiehuangbaobao/weddingservice/WeddingserviceApplication.java
@@ -0,0 +1,13 @@
+package cn.xiehuangbaobao.weddingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingserviceApplication.class, args);
+    }
+
+}
