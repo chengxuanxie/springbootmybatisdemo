af5ebf9db34ba7a6ac0010afd23401984eb425f2
Janna <504162491@qq.com>
2019-07-17 22:29:32 +0800

    feat(1.0.0):springboot demo with orm

--- src/main/java/cn/xiehuangbaobao/weddingservice/demo/controller/DemoController.java
+++ src/main/java/cn/xiehuangbaobao/weddingservice/demo/controller/DemoController.java
@@ -1,13 +1,20 @@
 package cn.xiehuangbaobao.weddingservice.demo.controller;
 
+import cn.xiehuangbaobao.weddingservice.demo.service.UserService;
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/demo")
     public String demo(){
         System.out.println("TestController的方法被调用了");
-        return "welcome to the new age !";
+        return JSON.toJSONString(userService.selectAll());
     }
 }
